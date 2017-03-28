6c4030b57c014d6e9475f1d4d6409b6eb4e08aaf
CuDecker <deckercu@msu.edu>
2017-03-28 15:47:18 -0400

    added working button and visit counter

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 var currentTheme = parseInt(window.localStorage.getItem('currentTheme'))
+var visits = parseInt(window.localStorage.getItem('visits'))
 
 var themeButton = document.getElementById('theme')
-
 var day = 1
-
 var night = 2
 
-if (currentTheme === null) {
+if (isNaN(visits)) {
+  visits = 0
+}
+
+if (visits > -1) {
+  visits = visits + 1
+  window.localStorage.setItem('visits', visits)
+  document.getElementById('visits').innerHTML = visits
+}
+
+if (currentTheme === null || isNaN(currentTheme)) {
   currentTheme = day
   document.body.setAttribute('class', 'daytime')
   console.log('no previous theme set, defaulting...')
 }
+
 if (currentTheme === day) {
   document.body.setAttribute('class', 'daytime')
   console.log('Previous day theme found, setting...')
-} else if (currentTheme === night) {
+} else {
   document.body.setAttribute('class', 'nightime')
   console.log('Previous night theme found, setting...')
 }
@@ -22,13 +32,13 @@ if (currentTheme === day) {
 themeButton.addEventListener('click', function () {
   if (currentTheme === day) {
     currentTheme = night
-    document.body.setAttribute('class', 'daytime')
+    document.body.setAttribute('class', 'nightime')
     window.localStorage.setItem('currentTheme', currentTheme)
     console.log('Was day, now night')
-  } else if (currentTheme === night) {
+  } else {
     currentTheme = day
     window.localStorage.setItem('currentTheme', currentTheme)
-    document.body.setAttribute('class', 'nightime')
+    document.body.setAttribute('class', 'daytime')
     console.log('was night, now day')
   }
 })
